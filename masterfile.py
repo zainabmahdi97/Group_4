495e827f4ba84eba6c5f74e613560cc6e4be7154
GioLopez23 <giolopezunam@gmail.com>
2023-03-01 10:29:49 -0800

    new file:   masterfile.py

---
+++ masterfile.py
@@ -0,0 +1,14 @@
+# Zainab Mahdi, Gianni Oquendo, Giovanni Lopez
+# CSE 5408
+# Group 4
+# Spring 2023
+# Lab 4
+
+# Part A: Output Reverse Input String - Gianni
+
+
+# Part B: Check If Entered Number Is Prime - Zainab
+
+
+# Part C: Output Current Time In Military Time - Giovanni
+
