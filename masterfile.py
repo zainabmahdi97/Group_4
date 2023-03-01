eb19f2d5d3e79dd3219eb7e399e54cc5aa7bc93f
Giannioq <giannioquendo@gmail.com>
2023-03-01 11:46:43 -0800

    masterfile

---
+++ masterfile.py
@@ -0,0 +1,35 @@
+# Zainab Mahdi, Gianni Oquendo, Giovanni Lopez
+# CSE 5408
+# Group 4
+# Spring 2023
+# Lab 4
+
+# Imports:
+from datetime import datetime
+
+# Part A: Output Reverse Input String - Gianni
+message = input("Enter a message: ")
+
+def reverse():
+	print(f"Message in reverse: {message[::-1]}")
+
+reverse()
+input()
+
+# Part B: Check If Entered Number Is Prime - Zainab
+num = int(input("input the number to check it "))
+if (num <= 1):
+    print("The number is not prime")
+else:
+    for i in range(2, num):
+        if (num%i == 0): # If the number has a divisor
+            print("The number is not prime")
+            break
+    else: # If the for loop ends without reaching any break
+        print("The number is prime")
+
+
+# Part C: Output Current Time In Military Time - Giovanni
+current = datetime.now()
+currenttime = current.strftime('%H%M')
+print('Current Time Is: ', currenttime)
