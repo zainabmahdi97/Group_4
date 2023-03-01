ce1cda8a39f88593515c20cc3076293024df5821
GioLopez23 <giolopezunam@gmail.com>
2023-03-01 13:33:44 -0800

    Updated masterfile.py

--- masterfile.py
+++ masterfile.py
@@ -8,9 +8,25 @@
 from datetime import datetime
 
 # Part A: Output Reverse Input String - Gianni
+message = input("Enter a message: ")
 
+def reverse():
+	print(f"Message in reverse: {message[::-1]}")
+
+reverse()
+input()
 
 # Part B: Check If Entered Number Is Prime - Zainab
+num = int(input("input the number to check it "))
+if (num <= 1):
+    print("The number is not prime")
+else:
+    for i in range(2, num):
+        if (num%i == 0): # If the number has a divisor
+            print("The number is not prime")
+            break
+    else: # If the for loop ends without reaching any break
+        print("The number is prime")
 
 
 # Part C: Output Current Time In Military Time - Giovanni
